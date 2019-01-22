78d6a30cbc9181d8f7d0a9514460adbd5e00fafc
nandagopalrl <nandagopalrl@users.noreply.github.com>
2019-01-22 22:02:46 +0000

    Initial project structure setup. All projects empty.

---
+++ WebShopExcercise.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebShopExcercise.WebUI.MvcApplication" Language="C#" %>
---
+++ WebShopExcercise.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebShopExcercise.WebUI.Startup))]
+namespace WebShopExcercise.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
