b31a77794b8595172c8cbac65439a1ab99b907cc
NANDAGOPAL RAJA LAKSHMINARAYANAN <rlnandha.2006@gmail.com>
2019-01-23 16:02:41 +0000

    Starting Product management classes

---
+++ WebShopExcercise.WebUI/Controllers/ProductManagerController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebShopExcercise.Core.Models;
+using WebShopExcercise.DataAccess.InMemory;
+
+
+namespace WebShopExcercise.WebUI.Controllers
+{
+    public class ProductManagerController : Controller
+    {
+        ProductRepository context;
+
+        public ProductManagerController()
+        {
+            context = new ProductRepository();
+        }
+
+        // GET: ProductManager
+        public ActionResult Index()
+        {
+            List<Product> products = context.Collection().ToList();
+            return View();
+        }
+    }
+}
