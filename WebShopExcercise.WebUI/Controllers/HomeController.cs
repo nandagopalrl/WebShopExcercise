cc80b46e29e2a441bd9aae8003484f72fd5821e9
NANDAGOPAL RAJA LAKSHMINARAYANAN <rlnandha.2006@gmail.com>
2019-01-31 15:48:10 +0000

    Product listing on homepage

--- WebShopExcercise.WebUI/Controllers/HomeController.cs
+++ WebShopExcercise.WebUI/Controllers/HomeController.cs
@@ -3,14 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebShopExcercise.Core.Contracts;
+using WebShopExcercise.Core.Models;
 
 namespace WebShopExcercise.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+        IRepository<Product> context;
+        IRepository<ProductCategory> productCategoryRepository;
+
+        public HomeController(IRepository<Product> productContext, IRepository<ProductCategory> productCategoryContext) // these are injected by the dependency injection class. check Unity.config
+        {
+            context = productContext;
+            productCategoryRepository = productCategoryContext;
+
+        }
         public ActionResult Index()
         {
-            return View();
+            List<Product> products = context.Collection().ToList();
+            return View(products);
+        }
+
+        public ActionResult Details(string id)
+        {
+            Product product = context.Find(id);
+
+            if(product != null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(product);
+            }
         }
 
         public ActionResult About()
