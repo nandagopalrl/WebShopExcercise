e0eb301618d10805a0f3a9341c972acbfc6cee91
NANDAGOPAL RAJA LAKSHMINARAYANAN <rlnandha.2006@gmail.com>
2019-02-04 15:39:21 +0000

    Added Basket and BasketItem Models

---
+++ WebShopExcercise.Core/Models/Basket.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebShopExcercise.Core.Models
+{
+    public class Basket : BaseEntity
+    {
+        public virtual ICollection<BasketItem> BasketItems { get; set; } //virtual for lazy loading in EF
+
+        public Basket()
+        {
+            this.BasketItems = new List<BasketItem>();
+        }
+    }
+}
